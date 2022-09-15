c47f015d7d2e72b773bd50faf613deb3d2197472
KutuzovMishail <cutuzow.mihail2016@yandex.ru>
2022-09-15 20:10:02 +0300

    The third point

--- lab1/lab1.py
+++ lab1/lab1.py
@@ -1,4 +1,11 @@
 import pandas as pd
 
 file = pd.read_csv('books.csv', encoding="windows-1251", delimiter=";")
-print(file['Название'])
+df1 = file['Название']
+
+count = 0
+
+for i in df1:
+    if len(i)>30:
+        count+=1
+print('Количество записей с названием длиннее 30 символов',count)
